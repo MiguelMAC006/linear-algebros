c45791c20f8652545007590c06e638995e132e3b
Miguel Cuevas <miguelcuevas006@gmail.com>
2026-04-26 16:31:09 -0400

    minor fix

--- supervised/supervised_learning.py
+++ supervised/supervised_learning.py
@@ -19,7 +19,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-import seaborn as sns
 
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
